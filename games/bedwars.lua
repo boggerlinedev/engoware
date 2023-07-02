5394848c57d4429040d0d7e7edfc723fa28b4f9b
boggerlinedev <101031002+boggerlinedev@users.noreply.github.com>
2023-07-02 15:15:29 +0800

    bedwars.lua

--- games/bedwars.lua
+++ games/bedwars.lua
@@ -259,15 +259,19 @@ end
 
 
 do 
+
+    local repstorage = game:GetService("ReplicatedStorage")
+    local Client = require(repstorage.TS.remotes).default.Client
+
     local NoFall = {}; NoFall = GuiLibrary.Objects.utilitiesWindow.API.CreateOptionsButton({
         Name = "nofall",
         Function = function(callback) 
             if callback then 
-               print("wat")
+               repeat
+        task.wait(0.5)
+        Client:Get("GroundHit"):SendToServer()
+    until  not NoFall.Enabled
 
-            else
-
-                print("fsdefsdfdf")
             end
         end,
     })
